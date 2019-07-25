9f3e26c7bee9d32e5f4c175e23735de5a70b2757
Yuri Victor <yurivictor@gmail.com>
2019-07-25 14:49:39 -0700

    Adding sandbox args option

--- index.js
+++ index.js
@@ -178,6 +178,8 @@ const captureWebsite = async (url, options) => {
 		launchOptions.slowMo = 100;
 	}
 
+	launchOptions.args = ['--no-sandbox', '--disable-setuid-sandbox']
+
 	const browser = options._browser || await puppeteer.launch(launchOptions);
 	const page = await browser.newPage();
 
